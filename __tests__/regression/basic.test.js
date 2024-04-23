6bb7a6ecf444cbe3712c99533f44264208317426
Enzo <enzomdonadelli2004@gmail.com>
2024-04-22 22:14:22 -0500

    Changes on basic.test.js

--- __tests__/regression/basic.test.js
+++ __tests__/regression/basic.test.js
@@ -22,5 +22,31 @@ describe('GET /', () => {
     expect(response.statusCode).toBe(404);
   });
 
+  it('responds with ...', async () => {
+    fetch.mockResponseOnce(JSON.stringify([{ id: 2, name: 'DemoSat' }]));
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain('DemoSat');
+  });
 });
 
+describe('GET /launch/:id', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it('should fetch launch details and render successfully', async () => {
+    const launchesData = [{ id: '1', name: 'FalsonSat' }];
+    const launchDetails = { id: '1', name: 'FalconSat', crew: [] };
+    const crewDetails = [];
+
+    fetch.mockResponseOnce(JSON.stringify(launchesData));
+    fetch.mockResponseOnce(JSON.stringify(launchDetails));
+
+    const response = await request(app).get('/launch/5eb87cd9ffd86e000604b32a');
+
+    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(fetch).toHaveBeenCalledWith('https://api.spacexdata.com/v5/launches/5eb87cd9ffd86e000604b32a');
+    expect(response.text).toContain("links");
+  });
+});
